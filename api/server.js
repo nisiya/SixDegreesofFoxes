bbab10d66ad22da40a40fd91f55065b3553db755
Thomas Magnusson <thomas.magnusson1@marist.edu>
2017-10-27 23:39:03 -0400

    rework the server stuff

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 
-const express        = require('express');
-// const MongoClient    = require('mongodb').MongoClient;
-const app            = express();
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
 
 const port = 8000;
 
