91a784a6f482f9ce1e09945196478be608c08124
Thomas Magnusson <thomas.magnusson1@marist.edu>
2017-10-28 00:42:41 -0400

    logins work

--- api/app/routes/routes.js
+++ api/app/routes/routes.js
@@ -14,4 +14,54 @@ module.exports = function(app, db) {
     });
   });
 
+  app.post('/register/np', (req, res) => {
+    const np = req.body; // non profit
+    db.query(`insert into Non_profits
+      (np_name, email, pass, summary) values (
+      '${np.name}', '${np.email}', '${np.password}', '${np.summary}')`, (err, pointer) => {
+      if(err) throw err;
+      res.send('redirect here, you\'re successful');
+    });
+  });
+
+  // log in for both
+  app.post('/login', (req, res) => {
+    const unknown = req.body; // user or np
+    db.query(`select email, pass from Users where email='${unknown.email}'`, (err, pointer) => {
+      if(err) throw err;
+      if(!pointer[0]) {
+        // it's not a user, try nonprofit
+        db.query(`select email, pass from Non_profits where email='${unknown.email}'`, (err, pointer) => {
+          if(err) throw err;
+          if(!pointer[0]) {
+            // neither user nor user
+            res.send("you're not registered with us");
+          } else {
+            // we have a valid non profit
+            const nppass = pointer[0].pass;
+            if(unknown.password == nppass) {
+              return res.send("Np logged in.");
+            } else {
+              return res.send("bad np pass");
+            }
+          }
+        });
+      } else {
+        // we have a valid user
+        const userpass = pointer[0].pass;
+        if(unknown.password == userpass) {
+          return res.send("User logged in.");
+        } else {
+          return res.send("bad user pass");
+        }
+      }
+    });
+  });
+
+  // create a challenge
+  // app.post('/challenge', (req, res) => {
+  //   const challenge = req.body;
+  //   //
+  // });
+
 };
