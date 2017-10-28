620928aa6566fae59a24129b9f27132f72309edd
Kai Wong <kai.wong@ibm.com>
2017-10-28 01:13:55 -0400

    Added more REST calls for Angular part

--- sixdegreesoffoxes/src/app/create-challenge.component.ts
+++ sixdegreesoffoxes/src/app/create-challenge.component.ts
@@ -7,11 +7,15 @@ import { RouterModule, Routes, Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class CreateChallengeComponent {
-  private name: String;
-  private email: String;
-  private password: String;
-  constructor(private router: Router) {}
+  private first_name: String;
+  private last_name: String;
+  private user_email: String;
+  private user_pass: String;
+  private user_pass_conf: String;
+  constructor(private router: Router) {
+    
+  }
   login = function() {
-    // Do database call to check if user exists in the database
+    
   }
 }
